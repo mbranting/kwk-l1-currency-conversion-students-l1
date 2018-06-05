f382294f16f5c2a6715863118fd34989037b6bd1
mbranting <mckenna.branting@gmail.com>
2018-06-05 22:45:41 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,7 +1,7 @@
 # Write your code here
 def usd_to_eur
   puts "What is the amount in dollars (defined to the hundreth place)?"
-  dollars = gets.chomp.to_i
+  dollars = gets.chomp
   puts "Amount in euros:"
   puts dollars * 0.85 
 end
